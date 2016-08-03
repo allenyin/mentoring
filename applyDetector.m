384831d61d1445a52cbcdfd7823eaedcfef50076
allenyin <allen.s.yin@gmail.com>
2016-08-03 14:29:15 -0400

    Added ability to go through a bunch of images

--- applyDetector.m
+++ applyDetector.m
@@ -4,8 +4,14 @@ maleFaceDetector = vision.CascadeObjectDetector('maleFaceDetector.xml');
 
 % Add your test image location to MATLAB path
 imDir = 'enter/your/path/to/testImages';
-addPath(imDir);
+%addPath(imDir);
 
 % apply the detector on a test image in your imDir named 'image1', for example
-imgName = 'image1';
-applyDetectorOnImg(maleFaceDetector, imgName, 'male face');
+
+s = dir(imDir);
+for i = 3:numel(s)
+    imgName = s(i).name;
+    applyDetectorOnImg(maleFaceDetector, imgName, 'male face');
+    w = waitforbuttonpress;
+    close all;
+end
